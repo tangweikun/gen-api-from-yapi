8535c98831f9d99dace1fabbe232e17a1f966a8b
tangweikun <819573105@qq.com>
2021-07-13 18:19:01 +0800

    :hammer: refactor core code

--- src/index.js
+++ src/index.js
@@ -2,13 +2,36 @@ const axios = require("axios");
 const fs = require("fs");
 const path = require("path");
 const inquirer = require("inquirer");
-const { last, get } = require("lodash");
+const { last, get, upperFirst } = require("lodash");
 const config = require("./config.js");
 
 // 从yapi获取接口信息数据
 function getApiDataFromYapi(_id, token, server) {
   const url = `${server}/api/interface/get?id=${_id}&token=${token}`;
   return axios.get(url);
+}
+
+// 生成 typescript 类型声明
+function genTypeDefinition(typeName, req_query) {
+  const queryTypes = req_query.map((x) => (x.required === "1" ? `${x.name}: any; // ${x.desc}` : `${x.name}?: any; // ${x.desc}`));
+  const typeDefinition = `type ${typeName} = {\n\t${queryTypes.join("\n\t")}\n}\n`;
+  return typeDefinition;
+}
+
+// 生成接口相关信息
+function genApiComment({ project_id, _id, server, title, username }) {
+  const titleComment = `// ${title}`;
+  const linkComment = `// ${server}/project/${project_id}/interface/api/${_id}`;
+  const authorNameComment = `// 接口对接人: ${username}`;
+  return `${titleComment}\n${linkComment}\n${authorNameComment}\n`;
+}
+
+// 生成接口函数
+function genApi({ functionName, typeName, prefix, apiPath, method }) {
+  const firstLine = `export async function ${functionName}(query: ${typeName}) {`;
+  const secondLine = `  return await request(\`${prefix}${apiPath}\`, \"${method}\", query);`;
+  const thirdLine = `}`;
+  return `${firstLine}\n${secondLine}\n${thirdLine}\n`;
 }
 
 async function genCode(project_id, _id) {
@@ -19,27 +42,18 @@ async function genCode(project_id, _id) {
   const { data, errcode, errmsg } = get(apiData, "data", {});
 
   if (errcode === 0) {
+    const { path: apiPath, method, title, req_query, username } = data;
+    const globalCode = `import { request } from 'services';\n\n`;
+    const commentCode = genApiComment({ project_id, _id, server, title, username });
+    const functionName = `_${last(apiPath.split("/"))}`;
+    const typeName = `I${upperFirst(last(apiPath.split("/")))}`;
+    const typeDefinitionCode = genTypeDefinition(typeName, req_query);
+    const functionCode = genApi({ functionName, typeName, prefix, apiPath, method });
+    const fileContent = `${globalCode}${commentCode}${typeDefinitionCode}${functionCode}`;
+
     const fileName = `${outputDir}/${outputFile}`;
     const baseSrc = path.resolve(__dirname);
     fs.mkdirSync(path.join(baseSrc, outputDir), { recursive: true });
-
-    const { path: apiPath, method, title, req_query, username } = data;
-    const importContent = `import { request } from 'services';\n`;
-    const titleComment = `// ${title}`;
-    const linkComment = `// ${server}/project/${project_id}/interface/api/${_id}`;
-    const authorNameComment = `// 接口对接人: ${username}`;
-    const functionName = `_${last(apiPath.split("/"))}`;
-
-    const queryTypes = req_query.map((x) =>
-      x.required === "1" ? `${x.name}: any` : `${x.name}?: any`
-    );
-    const query = `query: { ${queryTypes.join("; ")} }`;
-
-    const firstLine = `export async function ${functionName}(${query}) {`;
-    const secondLine = `  return await request(\`${prefix}/v1/schBranchNetwork/getSchoolsByNetwork\`, \"${method}\", query);`;
-    const thirdLine = `}`;
-    const fileContent = `${importContent}\n${titleComment}\n${linkComment}\n${authorNameComment}\n${firstLine}\n${secondLine}\n${thirdLine}\n`;
-
     fs.writeFileSync(path.join(baseSrc, fileName), fileContent);
   } else {
     console.log(`Error: ${errmsg}`);
--- yapi/api.ts
+++ yapi/api.ts
@@ -1,8 +1,21 @@
 import { request } from 'services';
 
-// 新增教练车接口
-// http://192.168.192.132:3000/project/203/interface/api/14549
+// 评价信息分页列表
+// http://192.168.192.132:3000/project/183/interface/api/20212
 // 接口对接人: shanminghui
-export async function _trainingcar(query: {  }) {
-  return await request(`/api/data-exchange/v1/schBranchNetwork/getSchoolsByNetwork`, "POST", query);
+type IPageList = {
+	coachname?: any; // 教练员姓名
+	evaluatetimeEnd?: any; // 评价时间（止）
+	evaluatetimeStart?: any; // 评价时间（起）
+	idcard?: any; // 身份证号
+	licnum?: any; // 车牌号
+	limit: any; // 每页条数
+	occupationno?: any; // 准教证号
+	overall?: any; // 总体满意度
+	page: any; // 当前页
+	part?: any; // 培训部分
+	type?: any; // 评价对象类型，1:教练员 2:培训机构
+}
+export async function _pageList(query: IPageList) {
+  return await request(`/api/jp-train-noncore-svc/v1/schEvaluation/pageList`, "GET", query);
 }
