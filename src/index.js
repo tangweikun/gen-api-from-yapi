a63e60390c3b1fdd9716265fa5e6414d4f88cb03
tangweikun <819573105@qq.com>
2021-07-14 16:30:54 +0800

    :hammer: refactor core code

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ const fs = require("fs");
 const path = require("path");
 const inquirer = require("inquirer");
 const { last, get, upperFirst } = require("lodash");
-const config = require("./config.js");
+const { SERVER, OUTPUT_DIR, OUTPUT_FILE, PROJECT_CONFIG } = require("./config.js");
 
 // 从yapi获取接口信息数据
-function getApiDataFromYapi(_id, token, server) {
-  const url = `${server}/api/interface/get?id=${_id}&token=${token}`;
+function getApiDataFromYapi(_id, token) {
+  const url = `${SERVER}/api/interface/get?id=${_id}&token=${token}`;
   return axios.get(url);
 }
 
@@ -19,9 +19,9 @@ function genTypeDefinition(typeName, req_query) {
 }
 
 // 生成接口相关信息
-function genApiComment({ project_id, _id, server, title, username }) {
+function genApiComment({ project_id, _id, title, username }) {
   const titleComment = `// ${title}`;
-  const linkComment = `// ${server}/project/${project_id}/interface/api/${_id}`;
+  const linkComment = `// ${SERVER}/project/${project_id}/interface/api/${_id}`;
   const authorNameComment = `// 接口对接人: ${username}`;
   return `${titleComment}\n${linkComment}\n${authorNameComment}\n`;
 }
@@ -34,30 +34,39 @@ function genApi({ functionName, typeName, prefix, apiPath, method }) {
   return `${firstLine}\n${secondLine}\n${thirdLine}\n`;
 }
 
-async function genCode(project_id, _id) {
-  const { projectConfig, outputDir, outputFile, server } = config;
-  const { token, prefix } = projectConfig[project_id];
+// 将生成内容写入文件
+function createFileAndWriteContent(fileContent) {
+  const fileName = `${OUTPUT_DIR}/${OUTPUT_FILE}`;
+  console.log("文件路径：" + path.join(process.cwd(), OUTPUT_DIR));
+  fs.mkdirSync(path.join(process.cwd(), OUTPUT_DIR), { recursive: true });
+  fs.writeFileSync(path.join(process.cwd(), fileName), fileContent);
+}
 
-  const apiData = await getApiDataFromYapi(_id, token, server);
+async function genContent(project_id, _id) {
+  const { token, prefix } = PROJECT_CONFIG[project_id];
+
+  const apiData = await getApiDataFromYapi(_id, token);
   const { data, errcode, errmsg } = get(apiData, "data", {});
 
   if (errcode === 0) {
     const { path: apiPath, method, title, req_query, username } = data;
     const globalCode = `import { request } from 'services';\n\n`;
-    const commentCode = genApiComment({ project_id, _id, server, title, username });
+    const commentCode = genApiComment({ project_id, _id, SERVER, title, username });
     const functionName = `_${last(apiPath.split("/"))}`;
     const typeName = `I${upperFirst(last(apiPath.split("/")))}`;
     const typeDefinitionCode = genTypeDefinition(typeName, req_query);
     const functionCode = genApi({ functionName, typeName, prefix, apiPath, method });
     const fileContent = `${globalCode}${commentCode}${typeDefinitionCode}${functionCode}`;
 
-    const fileName = `${outputDir}/${outputFile}`;
-    console.log("文件路径：" + path.join(process.cwd(), outputDir));
-    fs.mkdirSync(path.join(process.cwd(), outputDir), { recursive: true });
-    fs.writeFileSync(path.join(process.cwd(), fileName), fileContent);
-  } else {
-    console.log(`Error: ${errmsg}`);
+    return fileContent;
   }
+
+  console.log(`Error: ${errmsg}`);
+}
+
+async function genCode(project_id, _id) {
+  const fileContent = await genContent(project_id, _id);
+  createFileAndWriteContent(fileContent);
 }
 
 module.exports = function cli() {
